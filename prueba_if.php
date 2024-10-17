867894abeed7532aa70311dd5b868c1bfff97256
Miguel Giner <miguelginer9@gmail.com>
2024-10-17 17:15:25 +0200

    Add files via upload

---
+++ prueba_if.php
@@ -0,0 +1,12 @@
+<?php
+$nota1 = 7.5;
+$nota2 = 3.7;
+if ($nota1 > $nota2)
+{
+    echo "La nota del examen $nota1 es la mayor";
+}
+else
+{
+    echo "La nota del examen $nota2 es la mayor";
+}
+?>
